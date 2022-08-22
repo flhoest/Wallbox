0b3859222ce5e513538ad1281aec3ad370a58604
Frederic Lhoest <flhoest@gmail.com>
2022-08-22 08:52:08 +0200

    Create wbCredentials.php

---
+++ wbCredentials.php
@@ -0,0 +1,18 @@
+<?php
+
+	//                     .__   .__  ___.
+	//    __  _  _______   |  |  |  | \_ |__    ____ ___  ___
+	//    \ \/ \/ /\__  \  |  |  |  |  | __ \  /  _ \\  \/  /
+	//     \     /  / __ \_|  |__|  |__| \_\ \(  <_> )>    <
+	//      \/\_/  (____  /|____/|____/|___  / \____//__/\_ \
+	//                  \/                 \/              \/
+
+	$auth=array(
+			"username" => "<username>",
+			"password" => "<password>"
+			);
+
+	// Tokens are valid for 15 days. Think at renewing them !
+	$token="<token>";
+	$id="<charger_id>";
+?>
